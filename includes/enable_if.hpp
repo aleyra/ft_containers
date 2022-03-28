f3d6c688ac36fb5450bf78e178a6e57a155956ed
Lucille Burnet <lburnet@z1r6p5.42lyon.fr>
2022-03-28 11:55:09 +0200

    enable_if is just this ??

--- includes/enable_if.hpp
+++ includes/enable_if.hpp
@@ -1,4 +1,14 @@
 #ifndef ENABLE_IF_H
 # define ENABLE_IF_H
 
+namespace ft{
+	template<bool Cond, class T = void>
+	struct enable_if{};
+
+	template<class T>
+	struct enable_if<true, T>{
+		typedef T type;
+	};
+}
+
 #endif
