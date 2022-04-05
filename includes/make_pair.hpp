571b7d972469409ba9a36b1c9ee354f2e4bfc158
Lucille Burnet <lburnet@z1r6p3.42lyon.fr>
2022-04-05 15:12:34 +0200

    make_pair ok ?

--- includes/make_pair.hpp
+++ includes/make_pair.hpp
@@ -1,4 +1,12 @@
 #ifndef MAKE_PAIR_H
 # define MAKE_PAIR_H
 
+# include "pair.hpp"
+
+namespace ft{
+	template <class T1, class T2>
+	pair<T1, T2>	make_pair(T1 x, T2 y){
+		return (pair<T1, T2>(x, y));
+	}
+}
 #endif
