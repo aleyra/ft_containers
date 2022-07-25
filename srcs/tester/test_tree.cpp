22a043e8ab9149450047f6b1f01ddc019512a160
Lucille Burnet <lburnet@z1r3p1.42lyon.fr>
2022-07-25 13:40:46 +0200

    working on avl tree

--- srcs/tester/test_tree.cpp
+++ srcs/tester/test_tree.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "my_avl_tree.hpp"
+#include "my_tests.hpp"
+
+void	print_node(ft::node<ft::pair<int,char>> *n){
+	std::cout << "adresse de la node : " << n
+		<< "/ndata de la node : ";
+	display_pair_int_char(n->data);
+	std::cout << "adresse de rchild : " << n->rchild
+		<<"\nadresse de lchild : " << n->lchild << std::endl;
+}
 
 void test_tree(){
-	ft::my_avl_tree<int>	t(11);
+	ft::my_avl_tree<int, char>	t(10, 'a');
 
-	(void)t;
-
+	print_node(t->root);
+	t->insert(11, 'b');
+	print_node(t->root);
+	
 }
