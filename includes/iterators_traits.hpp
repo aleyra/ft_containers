e1df7420c8447cfd3611e96c2beeb449201a489f
Lucille Burnet <lburnet@z1r2p5.42lyon.fr>
2022-07-28 16:02:28 +0200

    working on avl tree

--- includes/iterators_traits.hpp
+++ includes/iterators_traits.hpp
@@ -26,7 +26,7 @@ namespace ft{
 	template <class T>
 	struct iterator_traits<const T*>{
 		typedef std::ptrdiff_t						difference_type;
-		typedef T								value_type;
+		typedef const T								value_type;//le const est important
 		typedef const T*						pointer;
 		typedef const T&						reference;
 		typedef	std::random_access_iterator_tag	iterator_category;
